c21a91824b0bce33770dcce352298f2d606866a0
The5thAxiom <samridh.anand.paatni@gmail.com>
2021-10-03 12:17:32 +0530

    stack.cpp updated

--- Stacks/stack.cpp
+++ Stacks/stack.cpp
@@ -1,19 +1,56 @@
-#include <stdio.h>
+#include<iostream>
 
-class Stack {
+class ArrayStack {
 private:
     int * array;
     int size;
     int topPointer;
 public:
-    Stack(size) {
+    ArrayStack(int size) {
         this->size = size;
         this->array = (int *)calloc(this->size, sizeof(int));
         this->topPointer = -1;
     }
+    void Push(int num) {
+        if (this->topPointer == this->size - 1) {
+            std::cout << "Stack full, cannot push " << num << std::endl;
+            return;
+        }
+        this->topPointer++;
+        this->array[this->topPointer] = num;
+    }
+    int Pop() {
+        if (this->topPointer == -1) {
+            std::cout << "Stack empty, cannot pop" << std::endl;
+            return 0;
+        }
+        int popped = this->array[this->topPointer];
+        this->array[this->topPointer] == 0;
+        this->topPointer--;
+        return popped;
+    }
+    void Print() {
+        std::cout << this->topPointer + 1 << " elements: ";
+        for (int i = 0; i <= this->topPointer; i++) {
+            std::cout << this->array[i] << " ";
+        }
+        std::cout << std::endl;
+    }
 };
 
 int main() {
-    Stack stk;
+    ArrayStack stk(3);
+    stk.Push(10);
+    stk.Print();
+    stk.Push(20);
+    stk.Print();
+    stk.Push(30);
+    stk.Print();
+    stk.Push(40);
+    stk.Print();
+    stk.Pop();
+    stk.Print();
+    stk.Push(50);
+    stk.Print();
     return 0;
 }
